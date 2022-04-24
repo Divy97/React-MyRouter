08281ce77fbf5a4889fa8c3476c4d9fa51ce7ec8
Divy Parekh <divyparekh1810@gmail.com>
2022-04-25 00:05:22 +0530

    Project Done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import {Route, Link, Switch, BrowserRouter, Routes} from 'react-router-dom'
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import User from './User';
+import Visit from './Visit';
+import NotFound from './NotFound';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const routing = (
+  <BrowserRouter>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/visit">Visit</Link></li>
+        <li><Link to="/User">User</Link></li>
+      </ul>
+    </div>
+    <Routes>
+       <Route exact path="/" element={<App />}/>
+       <Route path="/visit" element={<Visit />}/>
+       <Route path="/User" element={<User />}/>
+       <Route path="*" element={<NotFound />}/>
+    </Routes>
+  </BrowserRouter>
 );
+ReactDOM.render(routing, document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
